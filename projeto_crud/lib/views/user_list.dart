50b7453784cd730520be06fc12b9e4aa442721ab
Flavia <flavia_gudy@hotmail.com>
2023-07-03 17:39:05 -0300

    Criando página de lista

---
+++ projeto_crud/lib/views/user_list.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_crud/data/dummy_users.dart';
+
+class UserList extends StatelessWidget {
+  const UserList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const users = {...DUMMY_USERS};
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lista de Usuários'),
+      ),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (ctx, i) => Text(users.values.elementAt(i).name),
+      ),
+    );
+  }
+}
